2351a1b95f97560bcb86ce5feb33307c555f0e87
Julio Salceda <julio986610@gmail.com>
2022-03-16 13:47:57 -0700

    add documentation and formatting

--- generator/utilities.py
+++ generator/utilities.py
@@ -11,12 +11,12 @@ class Image:
         self.data = cv2.imread(path)
 
     def resize(self, width=None, height=None):
-        # set initial dimensions, width and height
+        # setting initial variables
         d = None
         inter = cv2.INTER_AREA
         (h, w) = self.data.shape[:2]
 
-        # return image if no params given
+        # returning image if no sizing param is given
         if width is None and height is None:
             return self.data
 
@@ -28,12 +28,16 @@ class Image:
             r = width / float(w)
             d = (width, int(h * r))
 
-        # return resized
+        # return resized numpy array
         resized = cv2.resize(self.data, d, interpolation=inter)
         return resized
 
     def preview(self, w=None, h=None):
+
+        # Resizing image to fit preview panel
         resized = self.resize(width=w, height=h)
+
+        # converting numpy array to Tk image
         pil_array = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
         img_tk = PILImage.fromarray(pil_array)
         image = ImageTk.PhotoImage(img_tk)
@@ -47,18 +51,14 @@ class Ascii(Image):
         self.data = self.generate_ascii()
 
     def generate_ascii(self):
-
         image = self.data
+
+        # "Pixelating" image to smooth out busy images
         height, width = image.shape[:2]
-        h, w = (int(height/13), int(width/10))
+        h, w = (int(height / 13), int(width / 10))
         temp = cv2.resize(image, (w, h), interpolation=cv2.INTER_LINEAR)
         output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
-        final = self.print_ascii(output)
-        return final
 
-    def print_ascii(self, reference):
-        result = np.zeros((reference.shape[0], reference.shape[1], 3), np.uint8)
-        for i in range(13, reference.shape[0], 13):
-            for j in range(0, reference.shape[1], 10):
-                result = cv2.putText(result, 'A', (j, i), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
-        return result
+        # Generate resulting image
+        final = print_ascii(output)
+        return final
