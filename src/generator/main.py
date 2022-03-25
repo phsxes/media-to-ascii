cda7f3391233202c19e3ab9235b113c38971a54b
Julio Salceda <julio986610@gmail.com>
2022-03-25 13:06:31 -0700

    moved implementation to generator package

---
+++ src/generator/main.py
@@ -0,0 +1,35 @@
+import media
+import typer
+import logging
+import sys
+import os
+
+# Log config
+LOG_FORMAT = "%(message)s"
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
+log = logging.getLogger()
+
+# CLI app instancing
+app = typer.Typer()
+
+
+# main execution
+@app.command()
+def main(input_path: str):
+    exists = os.path.exists(input_path)
+    if not exists:
+        log.info("ERROR: File does not exists or is not readable.")
+        return
+
+    valid = media.validate(input_path)
+    if valid['img']:
+        media.get_ascii_from_image(input_path)
+    elif valid['video']:
+        media.get_ascii_from_media(input_path)
+    else:
+        log.info("ERROR: File type not supported.")
+
+
+# CLI app loop
+if __name__ == '__main__':
+    app()
